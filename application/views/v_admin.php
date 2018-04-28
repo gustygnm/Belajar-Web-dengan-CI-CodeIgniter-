0aab4f99d739d63c7d234fbc27c994cffc2e418e
gustygnm <gusti.ngurah.mertayasa@gmail.com>
2018-04-28 12:02:46 +0800

    add v_admin.php

---
+++ application/views/v_admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo 
+	base_url(); ?>asset/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <!-- navbar -->
+
+    <!-- sidemenu -->
+
+    <!-- content -->
+    
+    <!-- footer -->
+
+    <script src="<?php echo base_url(); ?>asset/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
